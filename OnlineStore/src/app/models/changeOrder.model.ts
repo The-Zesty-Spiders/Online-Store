6bbdb5cb9ade82459428903ae5ebc9a047526182
mihail-nikolov <mihail.y.nikolov@abv.bg>
2017-09-08 17:37:10 +0300

    cleaned not needed files/folders

--- OnlineStore/src/app/models/changeOrder.model.ts
+++
@@ -1,9 +0,0 @@
-export class changeOrder   {
-    Id: number;
-    Finished: boolean;
-
-    constructor(id: number, finished: boolean) {
-        this.Id = id;
-        this.Finished = finished;
-    }
-}
--- OnlineStore/src/app/shared/services/bodyTypeRequest.model.ts
+++
@@ -1,9 +0,0 @@
-export class BodyTypeRequest {
-    ModelId: number;
-    MakeId: number;
-
-    constructor(makeId: number, modelId: number) {
-        this.MakeId = makeId;
-        this.ModelId = modelId;
-    }
-}
