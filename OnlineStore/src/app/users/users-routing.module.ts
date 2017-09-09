f6f00f53cac0331d199a034c600e0a3928e714bf
nadezhda-grozdeva <nadezhda.grozdeva@gmail.com>
2017-09-09 18:43:09 +0300

    Equal validator directive/change password added
    Equal validator directive for pass and confirmPass
    Change password functionality

--- OnlineStore/src/app/users/users-routing.module.ts
+++ OnlineStore/src/app/users/users-routing.module.ts
@@ -1,11 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { AdminManageOrdersComponent } from './admin-manage-orders/admin-manage-orders.component';
 import { AuthGuard } from './guards/auth.guard';
 import {LoginComponent} from './user-login/login.component';
 import { NgModule } from '@angular/core';
 import { UserDetailsComponent } from './user-details/user-details.component';
-import { UserOrdersDetailsComponent } from './user-orders-details/user-orders-details.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { ViewOrdersComponent } from './view-orders/view-orders.component';
 
